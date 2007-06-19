4fe561732a4a2eb1a13ec69a1bd9b0e10118e353
sieberst <sieberst@eiffel.com>
2007-06-19 08:15:55 +0000

    Changed run-time to allocate additional space each object to store the object ID.
    SVN: 69184@branches/capture_replay

--- Src/C/run-time/malloc.c
+++ Src/C/run-time/malloc.c
@@ -689,6 +689,12 @@ doc:	</routine>
 rt_public EIF_REFERENCE emalloc_size(uint32 ftype, uint32 type, uint32 nbytes)
 {
 	EIF_REFERENCE object;				/* Pointer to the freshly created object */
+	
+	//<SIES>
+	nbytes = nbytes + 8;
+	//</SIES>
+
+
 #ifdef ISE_GC
 	uint32 mod;							/* Remainder for padding */
 #endif
@@ -1094,6 +1100,7 @@ doc:	</routine>
 rt_public EIF_REFERENCE spmalloc(rt_uint_ptr nbytes, EIF_BOOLEAN atomic)
 {
 	EIF_REFERENCE object;		/* Pointer to the freshly created special object */
+	nbytes = nbytes + sizeof(EIF_NATURAL_64); //SIES, allocate space for an additional attribute.
 #ifdef ISE_GC
 	rt_uint_ptr mod;
 #endif
