3cf4c69a0aa3e561d126886fe4ed738297f66fe6
Jocelyn Fiat <jfiat@eiffel.com>
2020-05-29 08:34:07 +0000

    Merged revisions 104378:104384 from trunk.
    SVN: 104387@branches/Eiffel_20.05

--- Src/Delivery/scripts/docker/unix/build_porterpackage.sh
+++ Src/Delivery/scripts/docker/unix/build_porterpackage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ "$SVN_EIFFELSTUDIO_REPO_REVISION" = "" ]; then
-        set SVN_EIFFELSTUDIO_REPO_REVISION=HEAD
+if [ -z "$SVN_EIFFELSTUDIO_REPO_REVISION" ]; then
+        SVN_EIFFELSTUDIO_REPO_REVISION=HEAD
 fi
 
 var_dir=`pwd`/var
@@ -18,14 +18,23 @@ if [ -d "$var_dir/eiffel" ]; then
 	echo Use Eiffel installation from $var_dir/eiffel/...
         t_docker_vol_opts="-v $var_dir/eiffel:/home/eiffel/Eiffel"
 fi
+if [ -z "$SVN_ISE_REPO" ]; then
+	SVN_ISE_REPO=svn://svn.ise/ise_svn
+fi
+if [ -z "$SVN_EIFFELSTUDIO_REPO" ]; then
+        SVN_EIFFELSTUDIO_REPO=svn://svn.ise/mirrors/eiffelstudio
+fi
+echo Use EiffelStudio repo $SVN_EIFFELSTUDIO_REPO
+echo Use ISE repo $SVN_ISE_REPO
+
 docker run --rm \
         --name=eiffel-deliv-pp \
         -v $output_dir:/home/eiffel/deliv/output \
         ${t_docker_vol_opts} \
         --network host \
-        -e SVN_ISE_REPO=svn://svn.ise/ise_svn \
+        -e SVN_ISE_REPO=$SVN_ISE_REPO \
         -e SVN_ISE_BRANCH=$SVN_ISE_BRANCH \
-        -e SVN_EIFFELSTUDIO_REPO=svn://svn.ise/mirrors/eiffelstudio \
+        -e SVN_EIFFELSTUDIO_REPO=$SVN_EIFFELSTUDIO_REPO \
         -e SVN_EIFFELSTUDIO_REPO_REVISION=$SVN_EIFFELSTUDIO_REPO_REVISION \
         -e SVN_EIFFELSTUDIO_BRANCH=$SVN_EIFFELSTUDIO_BRANCH \
 	-e INCLUDE_ENTERPRISE=$include_enterprise \
