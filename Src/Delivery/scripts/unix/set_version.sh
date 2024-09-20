a4a6ec766d91027d105f9e353e6ed6d57c59af21
Jocelyn Fiat <git@djoce.net>
2024-09-20 18:30:59 +0200

    More information for the set_version.sh

--- Src/Delivery/scripts/unix/set_version.sh
+++ Src/Delivery/scripts/unix/set_version.sh
@@ -9,11 +9,19 @@
 
 cd $1 > /dev/null
 computed_build_id=`git rev-list HEAD | wc -l`
+computed_build_sha1=`git rev-parse HEAD`
 cd - > /dev/null
 
 if [ "$2" = "" -o "$3" = "" ]; then
 	echo $computed_build_id
 else
-	sed -e "s/$2/$computed_build_id/" $3 > tmp
+	sed -e "s/$2/$computed_build_id/g" $3 > tmp
 	mv tmp $3
+
+
+	#update the version_info ... if any
+	curr_date=$(date '+%Y-%m-%d')
+	sed -e "s/Version_info:\ STRING\ =\ \"[0-9a-zA-Z_\,\ \/\:()\.\=\-]*\"/Version_info:\ STRING\ =\ \"Build:\ $computed_build_id ($computed_build_sha1) , Compilation:\ ${curr_date}\"/g" $3 > tmp
+	mv tmp $3
+
 fi
