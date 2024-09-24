cad1e7b3511ab5071773cfb5762119b7045e0f62
Jocelyn Fiat <jfiat@eiffel.com>
2024-09-24 15:52:11 +0000

    Fixing issue with make_installations and WiX binaries.

--- Src/Delivery/scripts/windows/install/wix/install.bat
+++ Src/Delivery/scripts/windows/install/wix/install.bat
@@ -1,4 +1,4 @@
 mklink /J "C:\Program Files (x86)\Windows Installer XML v3" %~dp0v3.0
 mklink /J "C:\Program Files (x86)\MSBuild\Microsoft\WiX" %~dp0
 cd v3.0
-mklink /J bin %~dp0
+mklink /J bin %~dp0v3.0
