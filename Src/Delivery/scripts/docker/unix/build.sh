f259faaebb580404aade962042e0afdac7c64de7
Jocelyn Fiat <git@djoce.net>
2024-09-25 19:15:33 +0200

    Minor addition for deliv docker scripts

--- Src/Delivery/scripts/docker/unix/build.sh
+++ Src/Delivery/scripts/docker/unix/build.sh
@@ -23,11 +23,25 @@ case "$op" in
 	st*)
 		./build_standard_deliveries.sh $*
 		;;
+	cancel)
+		echo Cancel any running deliv container
+		docker kill eiffel-deliv-pp
+		docker kill eiffel-deliv-img-64bits
+		docker kill eiffel-deliv-img-32bits
+		;;
+	clean)
+		echo Clean PP and Images
+		./docker_op.sh pp vol st clean
+		./docker_op.sh i64 vol st clean
+		./docker_op.sh i32 vol st clean
+		;;
 	*)
 		echo "Usage: "
 		echo "  pp: build porterpackage"
 		echo "  beta: build beta deliveries"
 		echo "  std|standard  : build standard deliveries"
 		echo "  ent|enterprise: build enterprise deliveries"
+		echo "  cancel: cancel any running deliv execution"
+		echo "  clean: clean PP and Images"
 		;;
 esac
