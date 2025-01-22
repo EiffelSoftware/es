25ecf1cf463f691c7ed6f90efdf882a2b0df264f
Jocelyn Fiat <github@djoce.net>
2025-01-22 15:49:00 +0100

    Updated diagram tool icons for client/supplier links

--- Src/Delivery/studio/bitmaps/src/build_icons.sh
+++ Src/Delivery/studio/bitmaps/src/build_icons.sh
@@ -669,8 +669,9 @@ SVGadd $RES/window.svg $OVERLAY/new_nw.svg 9.svg
 SVGadd $RES/document.svg $OVERLAY/new_nw.svg 11.svg
 # metric
 # supplier link
-SVGadd ../18/10.svg $OVERLAY/new_w.svg 13.svg
+SVGadd ../18/23.svg $OVERLAY/new_w.svg 13.svg
 # aggregate supplier link
+SVGadd ../18/24.svg $OVERLAY/new_w.svg 14.svg
 # inheritance link
 # and
 # or
