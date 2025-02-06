033703c0d1ec720148abdbc753f59934c585f772
Jocelyn Fiat <jfiat@eiffel.com>
2025-02-06 13:06:11 +0000

    Fixed setting of DEFAULT_ES_ROOT

--- Src/Delivery/scripts/windows/set_deliv_branch.bat
+++ Src/Delivery/scripts/windows/set_deliv_branch.bat
@@ -2,10 +2,11 @@
 set STUDIO_VERSION_MAJOR_MINOR=%1
 
 ::set DEFAULT_PUBLIC_SVN=https://svn.eiffel.com/eiffelstudio/branches/Eiffel_%STUDIO_VERSION_MAJOR_MINOR%
-set DEFAULT_ES_ROOT=https://gitlab.com/eiffelsoftware/es.git
+
+if not defined DEFAULT_ES_ROOT set DEFAULT_ES_ROOT=https://gitlab.com/eiffelsoftware/es.git
 set DEFAULT_ES_BRANCH=Eiffel_%STUDIO_VERSION_MAJOR_MINOR%
 
-set DEFAULT_ISE_SVN=svn://svn.ise/ise_svn/branches/Eiffel_%STUDIO_VERSION_MAJOR_MINOR%
+if not defined DEFAULT_ISE_SVN set DEFAULT_ISE_SVN=svn://svn.ise/ise_svn/branches/Eiffel_%STUDIO_VERSION_MAJOR_MINOR%
 
 echo Version: %STUDIO_VERSION_MAJOR_MINOR%
 echo Public repository: %DEFAULT_ES_ROOT% branch %DEFAULT_ES_BRANCH%
